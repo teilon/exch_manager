c91f9a58e46cb03b609beafedc4fc77f4b57b6f4
teilon <teiloncw@gmail.com>
2020-09-26 11:13:47 +0600

    fix db config

--- db.py
+++ db.py
@@ -1,12 +1,13 @@
+import os
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-POSTGRES_HOST = '78.155.206.12'
-POSTGRES_PORT = '5432'
-POSTGRES_DATABASE = 'postgres_db'
-POSTGRES_USER = 'torn'
-POSTGRES_PASSWORD = 'helicopter'
+POSTGRES_HOST = os.environ['POSTGRES_HOST'] #'78.155.206.12'
+POSTGRES_PORT = os.environ['POSTGRES_PORT'] #'5432'
+POSTGRES_DATABASE = os.environ['POSTGRES_DB'] #'postgres_db'
+POSTGRES_USER = os.environ['POSTGRES_USER'] #'torn'
+POSTGRES_PASSWORD = os.environ['POSTGRES_PASSWORD'] #'helicopter'
 
 SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@{}:{}/{}'.\
     format(POSTGRES_USER,
